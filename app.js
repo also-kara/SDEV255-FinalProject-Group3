a13ceb2cc4ffbfb117d0409d71a602fe5d1ba59d
ejacobs32 <ejacobs32@ivytech.edu>
2023-09-25 19:00:44 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// initialize modules
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+// register view engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// app configuration
+app.use(express.static(path.join(__dirname, 'public')));
+
+// listen for requests
+const webDoor = app.listen(3000);
+
+
+// prossess response
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/login/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.get('/courses', (req, res) => {
+    res.render('course_list');
+});
+
+app.get('/courses/add', (req, res) => {
+    res.render('course_new');
+});
